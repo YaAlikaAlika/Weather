6a4d471d7f631d0af5962e4f4f1f8db386d3a4ec
Alina <ya.alika-alika@yandex.ru>
2026-02-05 18:32:33 +0300

    Initial Commit

---
+++ Weather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Алина on 05.02.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
